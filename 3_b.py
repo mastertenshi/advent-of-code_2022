52275aa61c9a7f28138a258547ec165cd6ce0bb1
Angel Mitev <angelmitev@protonmail.com>
2022-12-04 04:10:52 +0100

    Add Day 3 - part two

---
+++ 3_b.py
@@ -0,0 +1,34 @@
+text = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"""
+
+def find_common_char(x, y, z):
+    for c in x:
+        if c in y and c in z:
+            return c
+
+
+sum = 0
+groups = []
+for line in text.strip().split("\n"):
+    groups.append(line)
+    if len(groups) != 3:
+        continue
+    
+    common_char = find_common_char(groups[0], groups[1], groups[2])
+    if common_char.islower():
+        # a-z 1-26
+        sum += ord(common_char) - 96
+    else:
+        # A-Z 27-52
+        sum += ord(common_char) - 38
+        
+    groups.clear()
+
+
+print(sum)
