6c3f9dd4bf464d900c031fac7351114a81b2c8d0
Angel Mitev <angelmitev@protonmail.com>
2022-12-08 23:24:57 +0100

    Refactor day 7

--- 7.py
+++ 7.py
@@ -1,55 +1,66 @@
 from models import Folder, File
 import utils
 
-
-def find_folder(name, parent_folder):
-    folders = list(filter(lambda f: isinstance(f, Folder) and f.name == name, parent_folder.children))
-    if len(folders) == 1:
-        return folders[0]
-    return None
-
-
 text = utils.get_input("7.txt")
 
 root = Folder("/", None)
 curr_folder = root
 
-is_ls = False
-for line in text.split("\n")[1:]:
-    line = line.strip()
-    
-    if line.startswith("$ ls"):
-        is_ls = True
-        continue
-    
-    if line.startswith("$ cd"):
-        is_ls = False
-        location = line[line.rindex(" ")+1:]
-        if location == '..':
-            curr_folder = curr_folder.parent
-        elif location == '/':
-            curr_folder = root
-        else:
-            curr_folder = find_folder(location, curr_folder)
 
-    if is_ls:
-        if line.startswith("dir"):
-            folder = Folder(line.split(" ")[1], curr_folder)
-            curr_folder.children.append(folder)
-        else:
-            file = File(*line.split(" ")[::-1], parent=curr_folder)
-            curr_folder.children.append(file)
-            curr_folder.add_size(int(file.size))
+def cd(input_str):
+    global curr_folder
+
+    location = input_str[input_str.rindex(" ") + 1:]
+    if location == '..':
+        curr_folder = curr_folder.parent
+    elif location == '/':
+        curr_folder = root
+    else:
+        curr_folder = find_folder(location, curr_folder)
+
+
+def add_file(input_str):
+    if input_str.startswith("dir"):
+        folder = Folder(input_str.split(" ")[1], curr_folder)
+        curr_folder.children.append(folder)
+    else:
+        file = File(*input_str.split(" ")[::-1], parent=curr_folder)
+        curr_folder.children.append(file)
+        curr_folder.add_size(int(file.size))
+
+
+def find_folder(name, directory):
+    folders = list(filter(lambda f: isinstance(f, Folder) and f.name == name, directory.children))
+    if len(folders) == 1:
+        return folders[0]
+    return None
+
+
+def read_input():
+    is_ls = False
+    for line in text.split("\n")[1:]:
+        line = line.strip()
+
+        if line.startswith("$ cd"):
+            is_ls = False
+            cd(line)
+
+        if line.startswith("$ ls"):
+            is_ls = True
+            continue
+
+        if is_ls:
+            add_file(line)
 
 
 def part_one(folder):
-    sum = 0
+    size_sum = 0
     for f in folder.children:
         if isinstance(f, Folder):
-            sum += part_one(f)
+            size_sum += part_one(f)
             if f.size <= 100_000:
-                sum += f.size
-    return sum
+                size_sum += f.size
+    return size_sum
 
 
 def part_two(folder):
@@ -62,5 +73,7 @@ def part_two(folder):
     return dirs
 
 
-print(f"Sum: {part_one(root)}")
-print(f"Folder to delete: {sorted(part_two(root))[0]}")
+if __name__ == '__main__':
+    read_input()
+    print(f"Sum: {part_one(root)}")
+    print(f"Folder to delete: {sorted(part_two(root))[0]}")
