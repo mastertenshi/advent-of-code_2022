4d51bbd281447383569bb378f985a33e725d6140
Angel Mitev <angelmitev@protonmail.com>
2022-12-08 23:13:01 +0100

    Refactor day 5

--- 5.py
+++ 5.py
@@ -1,55 +1,58 @@
 from pprint import pprint
+import utils
 import re
 
-
-def get_input():
-    with open("input/5.txt", "r") as file:
-        return file.read()
+text = utils.get_input("5.txt")
 
 
-def get_stacks_count(text):
+def get_stacks_count():
     for line in text.split('\n'):
         if line.strip().startswith('1'):
             return int(line[line.strip().rfind(' '):].strip())
     return -1
 
 
-def get_stacks(text):
-    stacks = [[] for _ in range(get_stacks_count(text))]
-    
+def get_stacks():
+    stacks = [[] for _ in range(get_stacks_count())]
+
     for line in text.split('\n'):
         if not line.strip().startswith('['): break
-        
+
         for i in range(1, len(line), 4):
-            if line[i] != ' ': stacks[(i-1) // 4].append(line[i])
-        
+            if line[i] != ' ':
+                stacks[(i - 1) // 4].append(line[i])
+
     for i in range(0, len(stacks)):
         stacks[i].reverse()
-        
+
     return stacks
 
 
-def move_elements_first(stacks, text):
+def move_stacks():
+    stacks_1 = get_stacks()
+    stacks_2 = get_stacks()
+
     for line in text.split('\n'):
         if line.startswith('move'):
-            [move_count, fr0m, to] = list(map(lambda c: int(c), re.findall('\d+', line)))
+            [move_count, src, dest] = list(map(lambda c: int(c), re.findall('\\d+', line)))
+            src -= 1
+            dest -= 1
+
+            # First method  (normal stack)
             for _ in range(move_count):
-                stacks[to-1].append(stacks[fr0m-1].pop())
-    return stacks
+                stacks_1[dest].append(stacks_1[src].pop())
+
+            # Second method  (move multiple elements at a time)
+            stacks_2[dest] += stacks_2[src][-move_count:]  # add to destination
+            stacks_2[src] = stacks_2[src][0:-move_count]   # remove from source
+
+    return stacks_1, stacks_2
 
 
-def move_elements_second(stacks, text):
-    for line in text.split('\n'):
-        if line.startswith('move'):
-            [move_count, fr0m, to] = list(map(lambda c: int(c), re.findall('\d+', line)))
-            stacks[to-1] += stacks[fr0m-1][-move_count:]
-            stacks[fr0m-1] = stacks[fr0m-1][0:-move_count]
-    return stacks
+first, second = move_stacks()
 
+print("First method----")
+pprint(first)
 
-text = get_input()
-stacks_first = move_elements_first(get_stacks(text), text)
-stacks_second = move_elements_second(get_stacks(text), text)
-
-pprint(stacks_first)
-pprint(stacks_second)
+print("\nSecond method----")
+pprint(second)
